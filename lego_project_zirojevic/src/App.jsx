6d2524cb44d5bda37c380e7a316cf821bcb71d18
htl_fz_40 <114216192+htl-filip-40@users.noreply.github.com>
2026-03-09 20:59:29 +0100

    gefixt_startet_jz

--- lego_project_zirojevic/src/App.jsx
+++ lego_project_zirojevic/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './css/App.css'
 import Cart from "./components/Cart.jsx";
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
